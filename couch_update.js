a0f70b3874af8ac13b3d387d3e2f7519090cf2d1
nickbarth <nickbarth@live.com>
2014-04-20 23:24:32 -0600

    added design doc updater

--- couch_update.js
+++ couch_update.js
@@ -1,38 +1,26 @@
-var request = require('request'),
-    couch_url = 'http://localhost:5984/lego-news/',
-    design_doc = {
-      _id: '_design/news',
-      language: 'javascript',
-    };
+var couchapp = require('couchapp'),
+    ddocs;
 
-design_doc.views = {
-  all_news: ['map', function (doc) {
-    if (doc.type !== 'Photo') return;
-
-    emit(doc.id, doc);
-  }]
+ddoc = {
+  _id: '_design/news',
+  views: {},
+  language: 'javascript',
 };
 
-design_doc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
+ddoc.views.all_news = {
+  map: function (doc) {
+    if (doc.type !== 'Photo') return;
+    emit(doc.id, doc);
+  },
+  reduce: '_count'
+}
+
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {
   if (!userCtx.name)
     throw({'unauthorized': 'You must be logged in to create a new post.'});
 
-  if (newDoc._deleted !== true && newDoc.type === 'Photo' && newDoc.title.trim().length < 5)
-    throw('forbidden', 'You must enter a title longer than 5 characters.');
+  if (newDoc._deleted !== true && newDoc.type === 'Photo' && newDoc.title.trim().length < 4)
+    throw('forbidden', 'You must enter a title longer than 4 characters.');
 };
 
-Object.keys(design_doc.views).forEach(function  (key) {
-  var view = design_doc.views[key],
-      obj = {};
-
-  obj[view[0]] = view[1].toString();
-  design_doc.views[key] = obj;
-});
-
-design_doc.validate_doc_update = design_doc.validate_doc_update.toString();
-
-// console.log(JSON.stringify(design_doc));
-
-request.get(couch_url + design_doc.id, function (err, code, body) {
-  console.log(body);
-});
+module.exports = ddoc;
