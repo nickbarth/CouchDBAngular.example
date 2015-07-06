09df866984f184e2c8bb5e4cf676f0af17c8b239
NickBarth@live.com <nickbarth@live.com>
2015-07-05 20:32:35 -0600

    added enable cors script

---
+++ enable_cors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Admin User: http://localhost:5984/_utils
+HOST=http://admin:admin@localhost:5984
+
+curl -X PUT $HOST/_config/httpd/enable_cors -d '"true"'
+curl -X PUT $HOST/_config/cors/origins -d '"*"'
+curl -X PUT $HOST/_config/cors/credentials -d '"true"'
+curl -X PUT $HOST/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
+curl -X PUT $HOST/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
